9d871fa961c5f6d8030adf5b8b597f19f2b70161
Eduardo Gomes <edu_merckx@yahoo.com.br>
2024-10-14 22:01:38 -0300

    add "contas" model

---
+++ madr/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class Conta:
+    __tablename__ = 'contas'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    senha: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, onupdate=func.now(), server_default=func.now()
+    )
