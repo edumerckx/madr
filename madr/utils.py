67bced2eb8b641e61b02196752d03227cd27668d
Eduardo Gomes <edu_merckx@yahoo.com.br>
2024-10-23 13:00:01 -0300

    improving sanitize code

--- madr/utils.py
+++ madr/utils.py
@@ -2,7 +2,6 @@ import re
 
 
 def sanitize(input: str) -> str:
-    res = re.sub("[^a-zA-ZÀ-ú\s]", "", input)
-    res = re.sub('\s{2,}', ' ', res)
-    res = re.sub('^\s|\s*$', '', res)
-    return res.lower()
+    res = re.sub(r'[^a-zA-ZÀ-ú\s]', '', input)
+    res = re.sub(r'\s{2,}', ' ', res)
+    return res.strip().lower()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,15 @@
 from madr.utils import sanitize
 
+
 def test_sanitize():
     tests = {
-        "Machado de Assis": 'machado de assis',
-        "Manuel        Bandeira": 'manuel bandeira',
-        "Edgar Alan Poe         ": 'edgar alan poe',
-        "Androides Sonham Com Ovelhas Elétricas?": 'androides sonham com ovelhas elétricas',
-        "  breve  história  do tempo ": 'breve história do tempo',
-        "O mundo assombrado pelos demônios": 'o mundo assombrado pelos demônios',
+        'Machado de Assis': 'machado de assis',
+        'Manuel        Bandeira': 'manuel bandeira',
+        'Edgar Alan Poe         ': 'edgar alan poe',
+        'Androides Sonham Com Ovelhas Elétricas?': 'androides sonham com ovelhas elétricas',  # noqa: E501
+        '  breve  história  do tempo ': 'breve história do tempo',
+        'O mundo assombrado pelos demônios': 'o mundo assombrado pelos demônios',  # noqa: E501
     }
 
-    for k in tests:
-        assert sanitize(k) == tests[k]
+    for key, value in tests.items():
+        assert sanitize(key) == value
